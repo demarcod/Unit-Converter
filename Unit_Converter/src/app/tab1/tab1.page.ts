1fca0ae1d5980f32913e4f5e63fec3194c22d6c2
Mariah DeMarco <mdemarc7@uwo.ca>
2020-05-07 20:46:26 -0400

    volume set up

--- Unit_Converter/src/app/tab1/tab1.page.ts
+++ Unit_Converter/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { shadow } from '@ionic/core/dist/types/utils/transition/ios.transition';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  vol = "Litres";
+  cVol;
+  tap;
+
   constructor() {}
 
+
+  convert(){
+    
+  }
+
+  cupsToLitres(){
+
+  }
 }
+
+
