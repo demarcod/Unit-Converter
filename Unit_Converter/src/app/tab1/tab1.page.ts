413950b60d67807acebe7c30a0b7f463880efa26
Mariah DeMarco <mdemarc7@uwo.ca>
2020-05-07 21:57:23 -0400

    changed way of conversion

--- Unit_Converter/src/app/tab1/tab1.page.ts
+++ Unit_Converter/src/app/tab1/tab1.page.ts
@@ -12,27 +12,61 @@ export class Tab1Page {
   cVol = 'Cups';
   valu;
   ans;
+  rate;
 
   constructor() {}
 
 
-  convert(){
-    if(this.vol=='Litres' && this.cVol == 'Cups'){
-      this.litresToCups();
+  convert() {
+    if (this.vol == 'Litres' && this.cVol == 'Cups') {
+      this.rate = 4.22675;
     }
-    else if(this.vol=='Cups' && this.cVol == 'Litres'){
-      this.cupsToLitres();
+    else if (this.vol == 'Cups' && this.cVol == 'Litres') {
+      this.rate = 0.236588;
     }
-  }
-  litresToCups(){
+    else if (this.vol == 'Cups' && this.cVol == 'Teaspoons') {
+      this.rate = 48;
+    }
+    else if (this.vol == 'Teaspoons' && this.cVol == 'Cups') {
+      this.rate = 0.0208333;
+    }
+    else if (this.vol == 'Teaspoons' && this.cVol == 'Litres') {
+      this.rate = 0.00492892;
+    }
+    else if (this.vol == 'Litres' && this.cVol == 'Teaspoons') {
+      this.rate = 202.884 ;
+    }
+    else if (this.vol == 'Tablespoons' && this.cVol == 'Teaspoons') {
+      this.rate = 3;
+    }
+    else if (this.vol == 'Teaspoons' && this.cVol == 'Tablespoons') {
+      this.rate = 0.333333 ;
+    }
+    else if (this.vol == 'Tablespoons' && this.cVol == 'Cups') {
+      this.rate = 0.0625;
+    }
+    else if (this.vol == 'Cups' && this.cVol == 'Tablespoons') {
+      this.rate = 16;
+    }
+    else if (this.vol == 'Tablespoons' && this.cVol == 'Litres') {
+      this.rate = 0.0147868;
+    }
+    else if (this.vol == 'Litres' && this.cVol == 'Tablespoons') {
+      this.rate = 67.628;
+    } 
 
-    this.ans = this.valu / 3.51951;
+    this.ans = this.precise_round(this.valu * this.rate, 4);
+  }
 
-  }
-  cupsToLitres(){ 
-    this.ans = this.valu * 3.51951;
-  }
+  precise_round(num, dec){
+ 
+    if ((typeof num !== 'number') || (typeof dec !== 'number')) 
+  return false; 
   
+    var num_sign = num >= 0 ? 1 : -1;
+      
+    return (Math.round((num*Math.pow(10,dec))+(num_sign*0.0001))/Math.pow(10,dec)).toFixed(dec);
+  } 
 }
 
 
